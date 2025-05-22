f3ce6f81a2c64e3f7e27c36b37b518edeaab7b6e
JulianAvila15 <123836061+JulianAvila15@users.noreply.github.com>
2025-05-22 16:14:26 -0500

    Added Navigation bar that allows you to move between pages.

--- Personal Website/LoadTemplatedElements.js
+++ Personal Website/LoadTemplatedElements.js
@@ -1,11 +1,18 @@
 async function loadComponent(id, file) {
-  const res = await fetch(file);
+  const res = await fetch(file);  
   const text = await res.text();
   document.getElementById(id).innerHTML = text;
 }
 
+function loadCSS(href) {
+  const link = document.createElement(href);
+  link.rel = "stylesheet";
+  link.href = href;
+  document.head.appendChild(link);
+}
 window.addEventListener("DOMContentLoaded", () => {
-  loadComponent("header-placeholder", "TemplatedElements/Header.html");
+    loadComponent("header-placeholder", "TemplatedElements/Header.html");
   loadComponent("footer-placeholder", "TemplatedElements/Footer.html");
-  loadComponent("Navigation-Menu-Bar", )
+  loadComponent("Navigation-Menu-Bar", "TemplatedElements/NavigationMenu.html");
+    loadCSS("TemplatedElements/NavigationMenu.css");
 });
